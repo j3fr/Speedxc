bacbc215d6e21d45d4fd55b1edf493cab19bfdc1
ELMUSAUIDK <39737898+ELMUSAUIDK@users.noreply.github.com>
2018-07-05 13:47:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,90 +2,98 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var request = require('request');
 
-client.on('ready', () => {
-            client.user.setGame(`speedxcraft.com`,'https://www.youtube.com/?hl=FR');
-    console.log('I am ready!');
-});
+	client.on('message', async msg => {
+	var prefix = "^";
+	var user = msg.author;
+			var a = msg.guild.roles.find("name", 'Agar');
+		if(!a){
+        a = await msg.guild.createRole({
+		  name: "Agar",
+          color: "#ffffff",
+          permissions:[]
+		})
+		
+        }
+	    var m = msg.guild.roles.find("name", 'Minecraft');
+	if(!m){
+        m =  await msg.guild.createRole({
+		  name: "Minecraft",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var f = msg.guild.roles.find("name", 'Fortnite');
+		if(!f){
+        f =  await msg.guild.createRole({
+		  name: "Fortnite",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
+        var black = msg.guild.roles.find("name", 'Blacksquad');
+	if(!black){
+        black =  await msg.guild.createRole({
+		  name: "Blacksquad",
+          color: "#ffffff",
+          permissions:[]
+		})
+        }
 
-client.on('message', msg => {
-  if (msg.content === 'ايبي') {
-    msg.reply('play.speedxcraft.com');
-  }
-});
+		if (msg.content.startsWith(prefix +'addmerole')) {
 
+		if(!msg.channel.guild) return msg.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+msg.channel.send(`يرحي اختيار رتبة اللعبة الذي تريدها \n1- لعبة اقاريو :agario: \n2- لعبة ماين كرافت :minecraft: \n3- لعبة فورت نايت :fortnite: \n4- لعبة بلاك سكواد :blacksquad\n5- الغاء ❌ \n6- **لديك60 ثانية للاختيار **\n<@${msg.author.id}>`).then(res => {     
+     res.react(':agario:').then(r=>{     
+     res.react(':minecraft:').then(r=>{
+     res.react(':fortnite:').then(r=>{
+     res.react(':blacksquad:').then(r=>{
+     res.react('❌').then(r=>{
 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('447512522128490533').roles.find('name', 'rainbow').edit({color: 'RANDOM'}) // {By !M,S..❤#0254}
-    },100);
-});
+    let aaa = (reaction, user) => reaction.emoji.name === ':agario:' && user.id === msg.author.id;    
+    let mmm = (reaction, user) => reaction.emoji.name === ':minecraft:' && user.id === msg.author.id;
+    let fff = (reaction, user) => reaction.emoji.name === ':fortnite:' && user.id === msg.author.id;
+    let bbbb = (reaction, user) => reaction.emoji.name === ':blackquad:' && user.id === msg.author.id;
+    let ccc = (reaction, user) => reaction.emoji.name === '❌' && user.id === msg.author.id;
 
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.get('377989934897954816').roles.find('name', 'rainbow').edit({color: 'RANDOM'}) // {By !M,S..❤#0254}
-    },100);
-});
+    let aa = res.createReactionCollector(aaa, { maxMatches:1 , time: 20000 , });
+    let mm = res.createReactionCollector(mmm, { maxMatches:1 , time: 20000 , });
+    let ff = res.createReactionCollector(fff, { maxMatches:1 , time: 20000 , });
+    let bb = res.createReactionCollector(bbb, { maxMatches:1 , time: 20000 , });
+    let bl = res.createReactionCollector(bbbb,{ maxMatches:1 , time: 20000 , });
+    let cc = res.createReactionCollector(ccc, { maxMatches:1 , time: 20000 , });
 
-client.on('message', msg => {
-  if (msg.content === 'ip') {
-    msg.reply('play.speedxcraft.com');
-  } 
-});
-
-client.on('ready', () => {
-    setInterval(function(){ 
-        client.guilds.get('369324885677309952').roles.find('name', 'rainbow').edit({color: 'RANDOM'}) // {By !M,S..❤#0254}
-    },100);
-}); 
-client.on('message', msg => {
-  if (msg.content === 'shop') {
-    msg.reply('https://www.speedxcraft.com');
-  }
-});
-
-
-client.on('message', msg => {
-  if (msg.content === 'ويب') {
-    msg.reply('https://www.speedxcraft.com');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'web') {
-    msg.reply('https://www.speedxcraft.com');
-  }
-});
-
-client.on('message', msg => {
-  if (msg.content === 'web') {
-    msg.reply('موقعنا على الانترنت``speedxcraft.com``');
-  }
-});
-
-var mcIP = 'play.speedxcraft.com'; 
-
-client.on('message', message => {
-    if (message.content === '-status') {
-        var url = 'http://mcapi.us/server/status?ip=' + mcIP ;
-        request(url, function(err, response, body) {
-            if(err) {
-                console.log(err);
-                return message.reply('فيه خطا ');
-            }
-            body = JSON.parse(body);
-            var status = '*السيرفر طافي*';
-            if(body.online) {
-                status = '**السيرفر** **شغال**  -  ';
-                if(body.players.now) {
-                    status += '**' + body.players.now + '** واحد بالسيرفر';
-                } else {
-                    status += '*ولا حدا بيلعب بالسيرفر*';
-                }
-            }
-            message.reply(status);
-        });
-    }
-});
+aa.on("collect", r => {
+    msg.guild.member(user.id).addRole(a);
+	msg.channel.send('`تم اعطائك رتبة للعبة Agar`');
+	msg.delete();
+	})
+mm.on("collect", r => {
+    msg.guild.member(user.id).addRole(m);
+	msg.channel.send('`تم اعطائك رتبة للعبة Mincraft `');
+	msg.delete();
+})
+ff.on("collect", r => {
+    msg.guild.member(user.id).addRole(f);
+	msg.channel.send('`تم اعطائك رتبة للعبة Fortnite `');
+	msg.delete();
+})
+bl.on("collect", r => {
+    msg.guild.member(user.id).addRole(black);
+	msg.channel.send('`تم اعطائك رتبة للعبة Blacksquad `');
+	msg.delete();
+})
+cc.on("collect", r => {
+	msg.delete();
+})
+	 })
+	 })
+	 })
+	 })
+	 })
+	 })
+	 })
+	 }
+	 });
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
